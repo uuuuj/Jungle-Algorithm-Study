883e87374754366d1dba61408663786cc9a0c5c1
Sincer1ty <woodsy0417@gmail.com>
2025-05-20 22:30:31 +0900

    Create leetcode_3355.cpp

---
+++ week-05/seonghui/leetcode_3355.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> diff(nums.size(), 0);
+        
+        for (int i = 0; i < queries.size(); i++) {
+            int r = queries[i][1];
+            diff[queries[i][0]] -= 1;
+
+            if (r + 1 < nums.size()) diff[r + 1] += 1;
+        }
+
+        int apply = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            apply += diff[i];
+            if (nums[i] + apply > 0) return false;
+        }
+
+        return true;
+    }
+};
