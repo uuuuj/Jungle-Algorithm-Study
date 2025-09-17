2001edf0879a97feab406b6ae3d16ba510310b8b
Sincer1ty <woodsy0417@gmail.com>
2025-09-18 01:22:29 +0900

    Create baekjoon_20437.cpp

---
+++ week-22/seonghui/programmers_120898.cpp
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120898
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    return 2 * message.size();
+}
